fb041f79f150bad59fc7c2b5bff183c47e2aa084
wujg@calis.edu.cn <wujg@calis.edu.cn>
2020-11-03 10:23:05 +0800

    init2

---
+++ ClspMonitor/ClspMonitor/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for ClspMonitor project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/2.2/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'ClspMonitor.settings')
+
+application = get_wsgi_application()
